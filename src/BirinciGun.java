3c3ffdcce922146ebd428031f14a2d8e0c265fa6
aliyilmaz375 <aliyilmaz9182@gmail.com>
2020-11-17 00:01:56 +0300

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,8 +2,9 @@
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("11:57 - Birinci Gün");
+		System.out.println("Ýkinci Commit");
 	}
 
 }

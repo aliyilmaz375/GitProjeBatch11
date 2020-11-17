70f337ca6cefec2c5ad33d8ef4e27546259ebe15
aliyilmaz375 <aliyilmaz9182@gmail.com>
2020-11-17 22:28:39 +0300

    ikinci gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gün");
 		System.out.println("İkinci Commit");
 		System.out.println("10:11 - İkinci Gün");
+		System.out.println("10:26 ikinci gun");
 	}
 
 }

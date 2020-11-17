8610c2916cc7acfcc9c91fd3bf47e3f156c62cdb
aliyilmaz375 <74241283+aliyilmaz375@users.noreply.github.com>
2020-11-17 22:16:12 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,7 +5,7 @@ public class BirinciGun {
 		
 		System.out.println("11:57 - Birinci Gün");
 		System.out.println("İkinci Commit");
-		System.out.println("İkinci Gün");
+		System.out.println("10:11 - İkinci Gün");
 	}
 
 }

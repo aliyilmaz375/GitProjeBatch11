60b48e6cbecbfb917c73db193b1d7b2a5e8ec9bf
aliyilmaz375 <aliyilmaz9182@gmail.com>
2020-11-19 20:28:42 +0300

    message

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -12,7 +12,7 @@ public class BirinciGun {
 		System.out.println("10:46 - feature branch");
 		System.out.println("10:49 - feature branch");
 		System.out.println("10:49 - master branch");
-		
+		System.out.println("11:20 - feature branch");
 		
 		
 		

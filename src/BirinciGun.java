5cfe043729a99c9b742f53d159a02d1e6a21af88
aliyilmaz375 <aliyilmaz9182@gmail.com>
2020-11-19 17:50:53 +0300

    feature

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,13 +3,14 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 		
-		System.out.println("11:57 - Birinci Gün");
-		System.out.println("İkinci Commit");
-		System.out.println("10:11 - İkinci Gün");
+		System.out.println("11:57 - Birinci Gun");
+		System.out.println("ikinci Commit");
+		System.out.println("10:11 - ikinci Gun");
 		System.out.println("10:26 ikinci gun");
 		System.out.println("10:35 ikinci gun");
 		System.out.println("10:26 - feature branch");
 		System.out.println("10:46 - feature branch");
+		System.out.println("10:49 - feature branch");
 		
 		
 		
